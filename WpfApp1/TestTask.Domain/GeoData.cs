b9c2f255ed7806a2aab972d3ff752159e1374290
Kumodoushin <Kumodoushin@users.noreply.github.com>
2024-10-26 01:16:33 +0200

    first iteration of domain project

---
+++ WpfApp1/TestTask.Domain/GeoData.cs
@@ -0,0 +1,35 @@
+﻿using OneOf;
+using System.Net;
+
+namespace TestTask.Domain;
+
+public class GeoData
+{
+	public GeoData(OneOf<IPAddress,Uri> input)
+	{
+		IpAddress = input.IsT0 ? input.AsT0 : IPAddress.None;
+		Uri = input.IsT1 ? input.AsT1 : null;
+	}
+
+	internal GeoData()
+	{
+	}
+
+	public IPAddress IpAddress { get; internal set; }
+	public Uri? Uri { get; internal set; }
+	public string Latitude { get; internal set; }
+	public string Longitude { get; internal set; }
+
+	internal bool GeoDataIsFilled { get; set; } = false;
+
+	public void ApplyGeoDataFrom(IPGeolocation.Geolocation geolocation)
+	{
+		if (GeoDataIsFilled)
+		{
+			return;
+		}
+		Latitude = geolocation.GetLatitude();
+		Longitude = geolocation.GetLongitude();
+		GeoDataIsFilled = true;
+	}
+}
---
+++ WpfApp1/TestTask.Domain/IGeoDataRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace TestTask.Domain;
+
+public interface IGeoDataRepository
+{
+	Task<List<GeoData>> Get(CancellationToken cancellationToken);
+
+	Task Update(GeoData geoData, CancellationToken cancellationToken);
+
+	Task Delete(GeoData geoData, CancellationToken cancellationToken);
+}
